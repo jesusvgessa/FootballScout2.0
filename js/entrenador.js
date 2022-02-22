e10545fbfbe34a274e7d237431d2964758422ecf
jesusvgessa <jesusvgessa@hotmail.com>
2022-02-22 20:03:07 +0100

    entrenador

--- js/entrenador.js
+++ js/entrenador.js
@@ -1,83 +1,75 @@
-//Funcion para desplegable usuario
-function opcionesUser() {
-    var menuEmergente = document.getElementById("opcionesUsuario");
-    if (menuEmergente.style.display == "block") {
-        menuEmergente.style.display = "none";
-    } else {
-        menuEmergente.style.display = "block";
-    } //Fin Si
-}
+$(function() {
+    //Funcion para plantilla
+    //Evento multiple
+    function fueraFoco() {
+        var x = document.getElementById("filtroNombre");
+        x.value = x.value.toUpperCase();
+        x.style.background = "white";
+    }
 
-//Funcion para plantilla
-//Evento multiple
-function fueraFoco() {
-    var x = document.getElementById("filtroNombre");
-    x.value = x.value.toUpperCase();
-    x.style.background = "white";
-}
+    function dentroFoco(x) {
+        x.style.background = "orange";
+    }
 
-function dentroFoco(x) {
-    x.style.background = "orange";
-}
+    //Objetos
+    function Jugador(nombre, dorsal, posicion) {
+        // Propiedades
+        this.nombre = nombre;
+        this.dorsal = dorsal;
+        this.posicion = posicion;
+    }
 
-//Objetos
-function Jugador(nombre, dorsal, posicion) {
-    // Propiedades
-    this.nombre = nombre;
-    this.dorsal = dorsal;
-    this.posicion = posicion;
-}
+    var arrayJugadores = new Array();
+    var fichasJugador = document.querySelectorAll(".jugador");
 
-var arrayJugadores = new Array();
-var fichasJugador = document.querySelectorAll(".jugador");
-
-for (var i = 0; i < fichasJugador.length; i++) {
-    var nombre = fichasJugador[i].getElementsByTagName("p")[0].innerHTML;
-    var dorsal = fichasJugador[i].getElementsByTagName("p")[1].innerHTML;
-    var posicion = fichasJugador[i].getElementsByTagName("p")[2].innerHTML;
-    var jugador = new Jugador(nombre, dorsal, posicion);
-    arrayJugadores.push(jugador);
-} //Fin Para
-
-function buscaJugador() {
-    var nombre = document.getElementById("filtroNombre").value;
-    for (var i = 0; i < arrayJugadores.length; i++) {
-        if (!arrayJugadores[i].nombre.toLowerCase().includes(nombre.toLowerCase())) {
-            fichasJugador[i].style.display = "none";
-        } else {
-            fichasJugador[i].style.display = "block";
-        } //Fin Si
+    for (var i = 0; i < fichasJugador.length; i++) {
+        var nombre = fichasJugador[i].getElementsByTagName("p")[0].innerHTML;
+        var dorsal = fichasJugador[i].getElementsByTagName("p")[1].innerHTML;
+        var posicion = fichasJugador[i].getElementsByTagName("p")[2].innerHTML;
+        var jugador = new Jugador(nombre, dorsal, posicion);
+        arrayJugadores.push(jugador);
     } //Fin Para
-}
 
-function filtroPos() {
-    var pos = document.getElementById("pos").value;
-    if (pos != 0) {
+    function buscaJugador() {
+        var nombre = document.getElementById("filtroNombre").value;
         for (var i = 0; i < arrayJugadores.length; i++) {
-            if (arrayJugadores[i].posicion.toLowerCase() == (pos.toLowerCase())) {
-                fichasJugador[i].style.display = "block";
-            } else {
+            if (!arrayJugadores[i].nombre.toLowerCase().includes(nombre.toLowerCase())) {
                 fichasJugador[i].style.display = "none";
+            } else {
+                fichasJugador[i].style.display = "block";
             } //Fin Si
         } //Fin Para
-    } else {
-        for (var i = 0; i < arrayJugadores.length; i++) {
-            fichasJugador[i].style.display = "block";
-        } //Fin Para
     }
-}
 
-function filtroSelect() {
-    var filtro = document.getElementById("tema").value;
-    //Ordenar por select value
-}
+    function filtroPos() {
+        var pos = document.getElementById("pos").value;
+        if (pos != 0) {
+            for (var i = 0; i < arrayJugadores.length; i++) {
+                if (arrayJugadores[i].posicion.toLowerCase() == (pos.toLowerCase())) {
+                    fichasJugador[i].style.display = "block";
+                } else {
+                    fichasJugador[i].style.display = "none";
+                } //Fin Si
+            } //Fin Para
+        } else {
+            for (var i = 0; i < arrayJugadores.length; i++) {
+                fichasJugador[i].style.display = "block";
+            } //Fin Para
+        }
+    }
 
-//Anadir jugador
-var boton = document.getElementById("boton");
-if (arrayJugadores.length > 24) {
-    boton.style.display = "none";
-    console.log(arrayJugadores.length);
-} else {
-    boton.style.display = "block";
-    console.log(arrayJugadores.length);
-}
+    function filtroSelect() {
+        var filtro = document.getElementById("tema").value;
+        //Ordenar por select value
+    }
+
+    //Anadir jugador
+    var boton = document.getElementById("boton");
+    if (arrayJugadores.length > 24) {
+        boton.style.display = "none";
+        console.log(arrayJugadores.length);
+    } else {
+        boton.style.display = "block";
+        console.log(arrayJugadores.length);
+    }
+});
--- js/form.js
+++ js/form.js
@@ -13,3 +13,118 @@ Array.prototype.slice.call(forms)
             form.classList.add('was-validated')
         }, false)
     })
+
+window.onload = load;
+
+function load() {
+    //Entorno:
+    var registrar = document.getElementById("registro");
+
+    //Inputs
+    var nombre = document.getElementById("nombre");
+    var apes = document.getElementById("apes");
+    var email = document.getElementById("email");
+    var pass = document.getElementById("pass");
+    var pass2 = document.getElementById("pass2");
+    var error = document.getElementById("errores");
+
+    //Pattern:
+    var patternNombre = /(^[a-zA-ZñÑáéíóúÁÉÍÓÚ\s]{3,16})+$/;
+    var patternApes = /(^[a-zA-ZñÑáéíóúÁÉÍÓÚüÜ]{4,40}))+$/;
+    var patternEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    var patternPass = /^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,18}$/;
+
+    //Asigno un interruptor a cada variable para despues realizar la validación del formulario
+    var enviarNombre = false;
+    var enviarApes = false;
+    var enviarEmail = false;
+    var enviarPass = false;
+    var enviarPass2 = false;
+
+    //Eventos:
+    nombre.addEventListener("blur", validar);
+    apes.addEventListener("blur", validarApes);
+    email.addEventListener("blur", validarEmail);
+    pass.addEventListener("blur", validarPass);
+    pass2.addEventListener("blur", validarPass2);
+    registrar.addEventListener("submit", validarEnviar);
+
+    function borrarError() {
+        if (error.innerText.length != 0) {
+            error.removeChild(error.firstChild);
+        } //Fin Si
+    } //Fin Función
+
+    function validar() {
+        borrarError();
+        if (!patternNombre.test(this.value)) {
+            error.innerHTML = "ERROR: El nombre debe contener entre 3 y 16 letras*.";
+            nombre.setAttribute("class", "error");
+            enviarNombre = false;
+        } else {
+            nombre.removeAttribute("class");
+            enviarNombre = true;
+        } //Fin Si
+    } //Fin Funcion
+
+    function validarApes() {
+        borrarError();
+        if (!patternApes.test(this.value)) {
+            error.innerHTML = "ERROR: Debe introducir los 2 apellidos.";
+            apes.setAttribute("class", "error");
+            enviarApes = false;
+        } else {
+            apes.removeAttribute("class");
+            enviarApes = true;
+        } //Fin Si
+    } //Fin Funcion
+
+    function validarEmail() {
+        borrarError();
+        if (!patternEmail.test(this.value)) {
+            error.innerHTML = "ERROR: El email debe contener @ y .";
+            this.setAttribute("class", "error");
+            enviarEmail = false;
+        } else {
+            this.removeAttribute("class");
+            enviarEmail = true;
+        } //Fin Si
+    } //Fin Funcion
+
+    function validarPass() {
+        borrarError();
+        if (!patternPass.test(this.value)) {
+            error.innerHTML = "ERROR: La contraseña debe tener entre 8 y 18 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.";
+            this.setAttribute("class", "error");
+            enviarPass = false;
+        } else {
+            this.removeAttribute("class");
+            enviarPass = true;
+        } //Fin Si
+    } //Fin funcion
+
+    function validarPass2() {
+        borrarError();
+        if (pass.value != pass2.value) {
+            error.innerHTML = "ERROR: Las contraseñas deben ser iguales.";
+            this.setAttribute("class", "error");
+            enviarPass2 = false;
+        } else {
+            this.removeAttribute("class");
+            enviarPass2 = true;
+        } //Fin Si
+    } //Fin funcion
+
+    function validarEnviar(evento) {
+        if (!enviarUsuario || !enviarEmail || !enviarPass || !enviarPass2) {
+            borrarError();
+            error.innerHTML = "No se puede enviar el formulario con campos invalidos/vacios.";
+            evento.preventDefault();
+        } else {
+            var confirmacion = confirm("Crear usuario");
+            if (!confirmacion) {
+                evento.preventDefault();
+            } //Fin Si
+        } //Fin Si
+    } //Fin Funcion
+}
