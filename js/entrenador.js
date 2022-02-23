8d91216a4e746837d16b9ba77e5c6293de60f572
jesusvgessa <jesusvgessa@hotmail.com>
2022-02-23 01:21:31 +0100

    plantilla

--- js/entrenador.js
+++ js/entrenador.js
@@ -1,75 +1,66 @@
-$(function() {
-    //Funcion para plantilla
-    //Evento multiple
-    function fueraFoco() {
-        var x = document.getElementById("filtroNombre");
-        x.value = x.value.toUpperCase();
-        x.style.background = "white";
-    }
+//Objetos
+function Jugador(nombre, dorsal, posicion) {
+    // Propiedades
+    this.nombre = nombre;
+    this.dorsal = dorsal;
+    this.posicion = posicion;
+}
 
-    function dentroFoco(x) {
-        x.style.background = "orange";
-    }
+var arrayJugadores = new Array();
+var fichasJugador = document.querySelectorAll("#jugador");
 
-    //Objetos
-    function Jugador(nombre, dorsal, posicion) {
-        // Propiedades
-        this.nombre = nombre;
-        this.dorsal = dorsal;
-        this.posicion = posicion;
-    }
+fichasJugador.forEach(ficha => {
+    var nombre = ficha.getElementsByTagName("p")[0].innerHTML;
+    var dorsal = ficha.getElementsByTagName("p")[1].innerHTML;
+    var posicion = ficha.getElementsByTagName("p")[2].innerHTML;
+    var jugador = new Jugador(nombre, dorsal, posicion);
+    arrayJugadores.push(jugador);
+});
 
-    var arrayJugadores = new Array();
-    var fichasJugador = document.querySelectorAll(".jugador");
+//Filtro por posicion
+document.getElementById('pos').addEventListener("change", filtroPos);
 
-    for (var i = 0; i < fichasJugador.length; i++) {
-        var nombre = fichasJugador[i].getElementsByTagName("p")[0].innerHTML;
-        var dorsal = fichasJugador[i].getElementsByTagName("p")[1].innerHTML;
-        var posicion = fichasJugador[i].getElementsByTagName("p")[2].innerHTML;
-        var jugador = new Jugador(nombre, dorsal, posicion);
-        arrayJugadores.push(jugador);
-    } //Fin Para
-
-    function buscaJugador() {
-        var nombre = document.getElementById("filtroNombre").value;
+function filtroPos() {
+    var pos = document.getElementById("pos").value;
+    if (pos != 0) {
         for (var i = 0; i < arrayJugadores.length; i++) {
-            if (!arrayJugadores[i].nombre.toLowerCase().includes(nombre.toLowerCase())) {
-                fichasJugador[i].style.display = "none";
-            } else {
+            if (arrayJugadores[i].posicion.toLowerCase() == (pos.toLowerCase())) {
                 fichasJugador[i].style.display = "block";
+            } else {
+                fichasJugador[i].style.display = "none";
             } //Fin Si
         } //Fin Para
-    }
-
-    function filtroPos() {
-        var pos = document.getElementById("pos").value;
-        if (pos != 0) {
-            for (var i = 0; i < arrayJugadores.length; i++) {
-                if (arrayJugadores[i].posicion.toLowerCase() == (pos.toLowerCase())) {
-                    fichasJugador[i].style.display = "block";
-                } else {
-                    fichasJugador[i].style.display = "none";
-                } //Fin Si
-            } //Fin Para
-        } else {
-            for (var i = 0; i < arrayJugadores.length; i++) {
-                fichasJugador[i].style.display = "block";
-            } //Fin Para
-        }
-    }
-
-    function filtroSelect() {
-        var filtro = document.getElementById("tema").value;
-        //Ordenar por select value
-    }
-
-    //Anadir jugador
-    var boton = document.getElementById("boton");
-    if (arrayJugadores.length > 24) {
-        boton.style.display = "none";
-        console.log(arrayJugadores.length);
     } else {
-        boton.style.display = "block";
-        console.log(arrayJugadores.length);
+        for (var i = 0; i < arrayJugadores.length; i++) {
+            fichasJugador[i].style.display = "block";
+        } //Fin Para
     }
-});
+}
+
+//Filtro por nombre
+document.getElementById('filtroNombre').addEventListener('keyup', buscaJugador);
+
+function buscaJugador() {
+    var nombre = document.getElementById("filtroNombre").value;
+    for (var i = 0; i < arrayJugadores.length; i++) {
+        if (!arrayJugadores[i].nombre.toLowerCase().includes(nombre.toLowerCase())) {
+            fichasJugador[i].style.display = "none";
+        } else {
+            fichasJugador[i].style.display = "block";
+        } //Fin Si
+    } //Fin Para
+}
+
+//Evento multiple
+document.getElementById('filtroNombre').addEventListener('focusout', fueraFoco);
+document.getElementById('filtroNombre').addEventListener('focus', dentroFoco);
+
+function fueraFoco() {
+    var x = document.getElementById("filtroNombre");
+    x.value = x.value.toUpperCase();
+    x.style.background = "white";
+}
+
+function dentroFoco() {
+    this.style.background = "orange";
+}
--- js/modal.js
+++ js/modal.js
@@ -4,3 +4,14 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function() {
     myInput.focus()
 })
+
+(function(el) {
+    window.addEventListener("message", (event) => {
+        if (event.origin.startsWith("https://www.sofascore")) {
+            if (el.id === event.data.id) {
+                el.style.height = event.data.height + "px";
+            }
+        }
+    });
+})
+(document.getElementById("sofa-player-embed-360938"));
